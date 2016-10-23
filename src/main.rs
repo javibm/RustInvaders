ce299f13ba77576f46921059307ca963757031b7
Javi <javibm6@gmail.com>
2016-10-23 23:44:04 +0200

    Main Loop move to library

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,10 @@
 extern crate sdl2;
-#[macro_use]
-mod events;
 
-use sdl2::pixels::Color;
-
-struct_events!{
-    keyboard: {
-        key_escape: Escape,
-        key_up: Up,
-        key_down: Down
-    },
-    else: {
-        quit: Quit { .. }
-    }
-}
+mod phi;
+mod views;
 
 fn main() {
-    // Inicializamos sdl2
-    let sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    // Creamos la ventana
-    let window = video.window("Rust Invaders", 800, 600)
-        .position_centered().opengl()
-        .build().unwrap();
-
-    let mut renderer = window.renderer()
-        .accelerated()
-        .build().unwrap();
-
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
-    loop {
-        events.pump();
-
-        if events.now.quit || events.now.key_escape == Some(true) {
-             break;
-        }
-
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
-        renderer.present();
-    }    
+   ::phi::spawn("RustInvaders", |_| {
+        Box::new(::views::ViewA)
+    });
 }
